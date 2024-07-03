f966d5bef2e0bb49881edfdca67a9b9ecfa899ff
Aditya <ketan@creator-club.co>
2024-07-03 17:09:34 +0530

    Implemented Filters API

--- controller/collabSwipeRequests/collabSwipeRequestsRoutes.js
+++ controller/collabSwipeRequests/collabSwipeRequestsRoutes.js
@@ -4,10 +4,11 @@ const router = Router();
 
 const collabSwipeReq = require("./collabSwipeRequestsController");
 
-const {getRelevantCollabs}=require('./getRelevantCollabs');
+const getRelevantCollabs=require('./getRelevantCollabs');
 
-router.post("/getAllCollabs/:user_id", getRelevantCollabs);
+router.post("/getAllCollabs/:user_id", getRelevantCollabs.getRelevantCollabs);
 router.post("/sendCollabRequests", collabSwipeReq.sendCollabRequest);
 router.put("/updateCollabRequest", collabSwipeReq.updateAction);
+router.post("/filterCollabs/:user_id",getRelevantCollabs.filterCollabs);
 
 module.exports = router;
