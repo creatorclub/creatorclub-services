90527b2c5787c8e6c37f3a154e01d4a1c073a83d
Aditya <ketan@creator-club.co>
2024-07-11 13:48:09 +0530

    Updated the bookmark count functionality

--- controller/bookmark/bookmarkController.js
+++ controller/bookmark/bookmarkController.js
@@ -1,9 +1,12 @@
 const BookMarkModel = require("../../models/bookmarks/bookmarkModel");
+const Collab = require("../../models/collaborations/collabsModel");
 
 const bookmarkCollab = async (req, res) => {
   const { action, collab_id, user_id, timestamp } = req.body;
 
   const findUser = await BookMarkModel.findOne({ where: { user_id: user_id } });
+
+  const updateBookmarkCount=await Collab.findOne({where:{collab_id:collab_id}});
 
   if (action === 0) {
     let UpdatedbookmarkArr = findUser.dataValues.bookmarks;
@@ -27,6 +30,12 @@ const bookmarkCollab = async (req, res) => {
       { where: { user_id: user_id } }
     );
 
+    let currentCount=updateBookmarkCount.dataValues.bookmark_count;
+
+    let updateCount=currentCount-1;
+
+    await Collab.update({bookmark_count:updateCount},{where:{collab_id:collab_id}});
+
     return res.status(200).json({ message: "Bookmark removed", status: 200 });
   } else {
     let updatedArr = findUser.dataValues.bookmarks;
@@ -44,8 +53,13 @@ const bookmarkCollab = async (req, res) => {
         where: { user_id: user_id },
       }
     );
+    let currentCount=updateBookmarkCount.dataValues.bookmark_count;
 
-    return res.status(200).json({ message: "bookmark added", status: 200 });
+    let updateCount=currentCount+1;
+
+    await Collab.update({bookmark_count:updateCount},{where:{collab_id:collab_id}});
+
+    return res.status(200).json({ message: "bookmark added and bookmark count updated", status: 200 });
   }
 };
 
